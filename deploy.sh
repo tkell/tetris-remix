a2f72a4dbbfed84427efeb227236703809292ec6
Thor Kell <thor@tide-pool.ca>
2016-11-12 19:29:46 -0500

    Updat deploy and deprecation

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 echo "beginning rsync"
 rsync -r --exclude 'deploy.sh' --exclude '.git' \
-/home/thor/Code/tetris-remix/* tidepool@tide-pool.ca:/home/tidepool/www/tetris-remix
+/Users/thor/Code/tetris-remix/* tidepool@tide-pool.ca:/home/tidepool/www/tetris-remix
 echo "rsync complete!"
